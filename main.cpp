05c139d31968216672b58b9f53c44133ead1108c
ForgeDom <maxstill2006@gmail.com>
2024-05-11 17:11:31 +0300

    Hm ex3 done

--- main.cpp
+++ main.cpp
@@ -7,5 +7,26 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	return 0;
+    int num;
+    cout << "¬вед≥ть число: ";
+    cin >> num;
+
+    int res = 0;
+    int multiplier = 1;
+
+    for (; num != 0; num /= 10) {
+        int digit = num % 10;
+        if (digit != 3 && digit != 6) {
+            res += digit * multiplier;
+            multiplier *= 10;
+        }
+    }
+    if (res == 0) {
+        return 0;
+    }
+    else {
+        cout << res << endl;
+    }
+
+	return 0;	
 }
