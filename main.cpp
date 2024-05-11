6065c1175e38e36b1d298679fbcc9d1f98cd00de
ForgeDom <maxstill2006@gmail.com>
2024-05-11 16:56:14 +0300

    Hm ex1 done

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ using namespace std;
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+	int count = 0;
+	for (int i = 100; i <= 999; i++) {
+		int sot = i / 100;
+		int des = (i / 10) % 10;
+		int odun = i % 10;
 
+		if (sot == des || sot == odun || des == odun) {
+			count++;
+		}
+	}
+	cout << count;
 	return 0;
 }
