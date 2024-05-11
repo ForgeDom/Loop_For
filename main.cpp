d9bd2c7842e8e2375197ee2569837975ec5ac795
ForgeDom <maxstill2006@gmail.com>
2024-05-11 17:34:45 +0300

    Hm ex7 done

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@
 using namespace std;
 
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-	return 0;
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 1; i <= 1000; i++) {
+        if (i % a == 0 && i % b == 0) {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
 }
