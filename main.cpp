a6ce87c4ed9fcd45500b57ce35225f59aa39fcab
ForgeDom <maxstill2006@gmail.com>
2024-05-11 17:28:28 +0300

    Hm ex5 done

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <Windows.h>
-#include <ctime>
 using namespace std;
 
 int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-	return 0;
+    int A;
+    cout << "Введіть ціле число A: ";
+    cin >> A;
+
+    int suma = 0;
+
+    for (int temp = A; temp != 0; temp /= 10) {
+        suma += temp % 10;
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < 3; i++) {
+        sum += suma * suma * suma;
+    }
+
+    if (sum == A * A) {
+        cout << "Куб суми цифр цього числа дорівнює A * A." << endl;
+    }
+    else {
+        cout << "Куб суми цифр цього числа не дорівнює A * A." << endl;
+    }
+
+    return 0;
 }
