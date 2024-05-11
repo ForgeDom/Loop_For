0c34bbe8f0f604be726d20b6fbc7708714172bd1
ForgeDom <maxstill2006@gmail.com>
2024-05-11 17:15:33 +0300

    Hm ex4 done

--- main.cpp
+++ main.cpp
@@ -7,5 +7,16 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+    int A;
+    cout << "¬вед≥ть число: ";
+    cin >> A;
+
+    for (int B = 1; B * B <= A; B++) {
+        if (A % (B * B) == 0) {
+            if (A % (B * B * B) != 0) {
+                cout << B << endl;
+            }
+        }
+    }
 	return 0;
 }
